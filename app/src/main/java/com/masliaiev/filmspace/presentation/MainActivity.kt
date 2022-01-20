3c5e00671fa071ed0dee170235e873963171fa2b
MasliaievKirill <kirillmaslyaev@gmail.com>
2022-01-20 15:53:04 +0200

    Initial commit

---
+++ app/src/main/java/com/masliaiev/filmspace/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.masliaiev.filmspace.presentation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.masliaiev.filmspace.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
